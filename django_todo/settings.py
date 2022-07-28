de6c352a6cb4675ccbc06e54c13677f2dc746a68
Pat McDonald <patmcdonald@me.com>
2022-07-28 18:56:49 +0000

    create env.py, park SK

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5&)#(o34*f5a!!2&-ml7rbc)k$4&g%jhbx!2@$w0+v1o&#q^9_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
